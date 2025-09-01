875c9b3561883e58e48b84a45b9b12e39c9cce3d
ngwenya604 <ngwenyajunior604@gmail.com>
2025-09-01 16:22:09 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~JmkknKbL#8VMokxawwq9_b4-qkU1CHizhSjzez8zCZBMAjTQSJeo",  // add your Session Id here
+  SESSION_ID: "malvin~Q70yjQxY#9MVwNY1O9goJ-dhWBHzo-faMo395TJjGwrRbF7TyzHc",  // add your Session Id here
   
   OWNER_NUMBER: "263777143277", // put your phone number here
   
